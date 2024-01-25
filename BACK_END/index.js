e690089e1f1a12b1120ce44075ac79f0497e02c5
nimishsara12 <nimishsarathe561@gmail.com>
2024-01-25 18:12:50 +0530

    upgrade backend

--- BACK_END/index.js
+++ BACK_END/index.js
@@ -4,20 +4,23 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import cookieParser from "cookie-parser";
 import tourRoute from './routes/tours.js'
-import userRoute from './routes/users.js'
-import authRoute from './routes/auth.js'
-import reviewRoute from './routes/reviews.js'
-import bookingRoute from './routes/bookings.js'
+// import userRoute from './routes/users.js'
+// import authRoute from './routes/auth.js'
+// import reviewRoute from './routes/reviews.js'
+// import bookingRoute from './routes/bookings.js'
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 8000
-const corsOptions = {
-   origin: true,
-   credentials: true
-}
+const port = process.env.PORT || 8000;
+app.get('/',(req,res)=>{
+    res.send('api is working');
+});
+// const corsOptions = {
+//    origin: true,
+//    credentials: true
+// }
 
-mongoose.set("strictQuery", false)
+mongoose.set("strictQuery", false);
 const connect = async() => {
    try {
       await mongoose.connect(process.env.MONGO_URI, {
@@ -25,16 +28,18 @@ const connect = async() => {
          useUnifiedTopology: true
       })
 
-      console.log('MongoDB connected')
-   } catch (error) {
-      console.log('MongoDB connected failed')
+      console.log('MongoDB connected');
+   } catch (err) {
+      console.log('MongoDB connected failed');
    }
-}
+};
 
-app.use(express.json())
-app.use(cors(corsOptions))
-app.use(cookieParser())
-app.use('/tours',tourRoute);
+
+app.use(express.json());
+// app.use(cors(corsOptions))
+app.use(cors());
+app.use(cookieParser());
+app.use("/tours", tourRoute);
 // app.use("/api/v1/auth", authRoute)
 // app.use("/api/v1/tours", tourRoute)
 // app.use("/api/v1/users", userRoute)
